ce9faf8ef756318e6c6b428797b011a3f33ec938
Milind Mishra <milind.mishra4@gmail.com>
2021-11-19 23:17:27 +0530

    update

---
+++ lecture-001/Loops.java
@@ -0,0 +1,9 @@
+public class Loops {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i = i + 1;
+        }
+    }
+}
