726c64eed67075f48b8090d15a13ab86d7cd24e6
Milind Mishra <milind.mishra4@gmail.com>
2021-11-19 22:44:46 +0530

    updates

---
+++ lecture-001/inputOutput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class inputOutput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        sc.close();
+    }
+}
---
+++ lecture-001/variables.java
@@ -0,0 +1,11 @@
+public class variables {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+    }
+}
