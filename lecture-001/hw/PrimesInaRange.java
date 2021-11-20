5a43afc0b4931b1881bdafde82e428f1e8bf22aa
Milind Mishra <milind.mishra4@gmail.com>
2021-11-20 20:51:30 +0530

    primes in a range

---
+++ lecture-001/hw/PrimesInaRange.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimesInaRange {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
