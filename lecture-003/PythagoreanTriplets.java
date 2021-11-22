d39740fd1b22d861eef34475d46256d53e972d8e
Milind Mishra <milind.mishra4@gmail.com>
2021-11-22 23:55:25 +0530

    p tripplet

---
+++ lecture-003/PythagoreanTriplets.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+// print if 
+public class PythagoreanTriplets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        isPythagoreanTriplets(a, b, c);
+        sc.close();
+    }
+
+    public static void isPythagoreanTriplets(int a, int b, int c) {
+        if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a) {
+            System.out.println("true");
+        } else {
+            System.out.println("flase");
+        }
+    }
+
+}
