69ba67e393b588809dbe0b248580796c8e315ed1
Milind Mishra <milind.mishra4@gmail.com>
2021-11-23 01:30:19 +0530

    inverse of a number

--- lecture-003/InverseNumber.java
+++ lecture-003/InverseNumber.java
@@ -4,13 +4,19 @@ public class InverseNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        printInverse(n);
+        int ans = printInverse(n);
+        System.out.println(ans);
         sc.close();
     }
 
-    public static void printInverse(int num) {
-        int ans = 0;
-        // DOING
-        System.out.println(ans);
+    public static int printInverse(int num) {
+        int lastIndex = 1, res = 0;
+        while (num != 0) {
+            int lastDigit = num % 10;
+            num /= 10;
+            res += lastIndex * (int) Math.pow(10, lastDigit - 1);
+            lastIndex++;
+        }
+        return res;
     }
 }
