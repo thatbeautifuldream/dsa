8713609efbc4ca6478fdd661b9b451687befa2c5
Milind Mishra <milind.mishra4@gmail.com>
2021-11-23 23:41:55 +0530

    updates

--- lecture-004/Pattern.java
+++ lecture-004/Pattern.java
@@ -1,18 +1,73 @@
 import java.util.Scanner;
 
 public class Pattern {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int rows = sc.nextInt();
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < rows; j++) {
-                System.out.print(" ");
+
+    public static void pattern1(int n) {
+        int nsp = n, nst = n;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
             }
-            for (int k = 0; k < rows; k++) {
-                System.out.print("*");
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
             }
+            nsp += 0;
+            nst += 0;
             System.out.println();
         }
+
+    }
+
+    public static void pattern2(int n) {
+        int nsp = n - 1, nst = 1;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
+            }
+            nsp--;
+            nst++;
+            System.out.println();
+        }
+    }
+
+    public static void pattern5(int n) {
+        int nsp = n - 1, nst = 1;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
+            }
+            nsp--;
+            nst += 2;
+            System.out.println();
+        }
+    }
+
+    public static void pattern6(int n) {
+        int nsp = 0, nst = 2 * n - 1;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
+            }
+            nsp++;
+            nst -= 2;
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        pattern6(n);
         sc.close();
     }
+
 }
