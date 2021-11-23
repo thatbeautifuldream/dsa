88eb10ed74115f81b9f7ff6f2ad9fddf5a2474f3
Milind Mishra <milind.mishra4@gmail.com>
2021-11-24 00:24:35 +0530

    changes

--- lecture-004/Pattern.java
+++ lecture-004/Pattern.java
@@ -63,10 +63,30 @@ public class Pattern {
         }
     }
 
+    public static void pattern7(int n) {
+        int nsp = n - 2, nst = 1;
+        for (int r = 1; r <= n; r++) {
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print("*\t");
+            }
+            if (r < n / 2) {
+                nsp--;
+                nst += 2;
+            } else {
+                nsp++;
+                nst -= 2;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern6(n);
+        pattern7(n);
         sc.close();
     }
 
