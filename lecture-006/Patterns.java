0d4729b351c1946c9777c387304bfda702840840
Milind Mishra <milind.mishra4@gmail.com>
2021-11-25 22:50:45 +0530

    pat 11 12

--- lecture-006/Patterns.java
+++ lecture-006/Patterns.java
@@ -2,6 +2,38 @@ import java.util.Scanner;
 
 public class Patterns {
 
+    public static void pattern11(int num) {
+        int nst = 1;
+        int val = 1;
+        for (int r = 1; r <= n; r++) {
+            // stars
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(val + "\t");
+                val++;
+            }
+            nst++;
+            System.out.println();
+        }
+
+    }
+
+    public static void pattern12(int n) {
+        int nst = 1;
+        int a = 0, b = 1;
+        for (int r = 1; r <= n; r++) {
+            // stars
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(a + "\t");
+                int c = a + b;
+                a = b;
+                b = c;
+            }
+            nst++;
+            System.out.println();
+        }
+
+    }
+
     public static void pattern14(int n) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + " * " + i + " = " + (n * i));
@@ -89,12 +121,37 @@ public class Patterns {
         }
     }
 
-    
+    public static void pattern16(int n) {
+        int nst = 1, nsp = 2 * n - 3;
+
+        for (int r = 1; r <= n; r++) {
+            int count = 1;
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(count + "\t");
+                count = (cst <= nst / 2) ? count + 1 : count - 1;
+            }
+
+            for (int csp = 1; csp <= nsp; csp++) {
+                System.out.print("\t");
+            }
+
+            if (r == n)
+                nst--;
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(count + "\t");
+            }
+
+            nst++;
+            nsp -= 2;
+            System.out.println();
+        }
+
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern15(n);
+        pattern16(n);
         sc.close();
     }
 }
