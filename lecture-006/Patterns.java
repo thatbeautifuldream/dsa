34b25c6802d5e0e727b69fbe4f4b061958060732
Milind Mishra <milind.mishra4@gmail.com>
2021-11-26 00:41:23 +0530

    changes

--- lecture-006/Patterns.java
+++ lecture-006/Patterns.java
@@ -34,6 +34,29 @@ public class Patterns {
 
     }
 
+    public static int nCr(int n, int r) {
+        return fact(n) / (fact(r) * fact(n - r));
+    }
+
+    // Returns factorial of n
+    public static int fact(int n) {
+        int res = 1;
+        for (int i = 2; i <= n; i++)
+            res = res * i;
+        return res;
+    }
+
+    public static void pattern13(int n) {
+        int nst = 1;
+        for (int r = 1; r <= n; r++) {
+            // stars
+            for (int cst = 1; cst <= nst; cst++) {
+                System.out.print(nCr(r - 1, cst) + "\t");
+            }
+            System.out.println();
+        }
+    }
+
     public static void pattern14(int n) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(n + " * " + i + " = " + (n * i));
@@ -151,7 +174,7 @@ public class Patterns {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        pattern16(n);
+        pattern13(n);
         sc.close();
     }
 }
