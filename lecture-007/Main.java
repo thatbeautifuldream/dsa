349aacdd83bbbdfe994e2b938d4fd81d5cde8137
Milind Mishra <milind.mishra4@gmail.com>
2021-11-26 23:07:15 +0530

    changes

--- lecture-007/Main.java
+++ lecture-007/Main.java
@@ -1,10 +1,11 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int ans = anyBaseToDecimal(n, 2);
+        int m = sc.nextInt();
+        int ans = binaryAddition(n, m);
         System.out.println(ans);
         sc.close();
     }
@@ -54,4 +55,39 @@ public class Main {
         return ans;
     }
 
+    public static int anyBaseToAnyBase(int n, int b1, int b2) {
+        int ab2d = anyBaseToDecimal(n, b2);
+        int ans = decimalToAnyBase(ab2d, b1);
+        return ans;
+    }
+
+    public static int decimalAddition(int num1, int num2) {
+        int ans = 0, carry = 0;
+        while (num1 != 0 || num2 != 0) {
+            int r1 = num1 % 10;
+            num1 /= 10;
+            int r2 = num2 % 10;
+            num2 /= 10;
+            int sum = r1 + r2 + carry;
+            carry = sum / 10;
+            sum %= 10;
+            ans = (ans * 10) + sum;
+        }
+        return ans;
+    }
+
+    public static int binaryAddition(int num1, int num2) {
+        int ans = 0, carry = 0;
+        while (num1 != 0 || num2 != 0) {
+            int r1 = num1 % 2;
+            num1 /= 2;
+            int r2 = num2 % 2;
+            num2 /= 2;
+            int sum = r1 + r2 + carry;
+            carry = sum / 2;
+            sum %= 2;
+            ans = (ans * 2) + sum;
+        }
+        return ans;
+    }
 }
