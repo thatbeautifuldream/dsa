5b1dd5afbfe9198e4f5e647371f044388e0b99fe
Milind Mishra <milind.mishra4@gmail.com>
2021-11-26 23:42:48 +0530

    changes

--- lecture-007/Main.java
+++ lecture-007/Main.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int ans = binaryAddition(n, m);
+        int ans = decimalAddition(sc.nextInt(), sc.nextInt());
         System.out.println(ans);
         sc.close();
     }
@@ -61,33 +59,48 @@ public class Main {
         return ans;
     }
 
-    public static int decimalAddition(int num1, int num2) {
-        int ans = 0, carry = 0;
-        while (num1 != 0 || num2 != 0) {
-            int r1 = num1 % 10;
-            num1 /= 10;
-            int r2 = num2 % 10;
-            num2 /= 10;
-            int sum = r1 + r2 + carry;
+    // public static int decimalAddition(int num1, int num2) {
+    // int ans = 0, carry = 0;
+    // while (num1 != 0 || num2 != 0) {
+    // int r1 = num1 % 10;
+    // num1 /= 10;
+    // int r2 = num2 % 10;
+    // num2 /= 10;
+    // int sum = r1 + r2 + carry;
+    // carry = sum / 10;
+    // sum %= 10;
+    // ans = (ans * 10) + sum;
+    // }
+    // return ans;
+    // }
+
+    // public static int binaryAddition(int num1, int num2) {
+    // int ans = 0, carry = 0;
+    // while (num1 != 0 || num2 != 0 || carry != 0) {
+    // int r1 = num1 % 2;
+    // num1 /= 2;
+    // int r2 = num2 % 2;
+    // num2 /= 2;
+    // int sum = r1 + r2 + carry;
+    // carry = sum / 2;
+    // sum %= 2;
+    // ans = (ans * 2) + sum;
+    // }
+    // return ans;
+    // }
+
+    public static int decimalAddition(int n, int m) {
+        int ans = 0, carry = 0, pow = 1;
+        while (n != 0 || m != 0 || carry != 0) {
+            int sum = n % 10 + m % 10 + carry;
+            n /= 10;
+            m /= 10;
             carry = sum / 10;
             sum %= 10;
-            ans = (ans * 10) + sum;
+            ans += sum * pow;
+            pow *= 10;
         }
         return ans;
     }
 
-    public static int binaryAddition(int num1, int num2) {
-        int ans = 0, carry = 0;
-        while (num1 != 0 || num2 != 0) {
-            int r1 = num1 % 2;
-            num1 /= 2;
-            int r2 = num2 % 2;
-            num2 /= 2;
-            int sum = r1 + r2 + carry;
-            carry = sum / 2;
-            sum %= 2;
-            ans = (ans * 2) + sum;
-        }
-        return ans;
-    }
 }
