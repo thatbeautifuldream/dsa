a081d5f9e37e78bb7d349b7dcc80949f6d7c38a7
Milind Mishra <milind.mishra4@gmail.com>
2021-11-27 20:55:53 +0530

    doubt class

---
+++ lecture-008-doubt-class/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+    }
+
+}
