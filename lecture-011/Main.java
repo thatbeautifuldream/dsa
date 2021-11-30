bab5e1b9cd6bd83c916f963e780fe77f3b858bbd
Milind Mishra <milind.mishra4@gmail.com>
2021-11-30 21:43:46 +0530

    update

--- lecture-011/Main.java
+++ lecture-011/Main.java
@@ -42,3 +42,37 @@ public class Main {
     }
 
 }
+
+// Questions :
+// SPAN OF ARRAY :
+// import java.util.Scanner;
+
+// public class Main {
+// // span of array : max - min
+// // diffrence of max and min
+// public static int spanOfArray(int[] arr) {
+// int max = Integer.MIN_VALUE;
+// int min = Integer.MAX_VALUE;
+// for(int i = 0; i < arr.length; i++) {
+// if(arr[i] < min) {
+// min = arr[i];
+// }
+// if(arr[i] > max) {
+// max = arr[i];
+// }
+// }
+// return (max - min);
+// }
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// int n = sc.nextInt();
+// int[] arr = new int[n];
+// for (int i = 0; i < n; i++) {
+// arr[i] = sc.nextInt();
+// }
+// System.out.println(spanOfArray(arr));
+// sc.close();
+// }
+
+// }
