ef1b6fee9fcde95473b20929ce00d9f723245a5b
Milind Mishra <milind.mishra4@gmail.com>
2021-11-30 22:40:56 +0530

    update

--- lecture-011/Main.java
+++ lecture-011/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static int maximum(int[] arr) {
         int max = Integer.MIN_VALUE;
+        // max = -(int)1e8;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
@@ -14,6 +15,7 @@ public class Main {
 
     public static int minimum(int[] arr) {
         int min = Integer.MAX_VALUE;
+        // min = (int)1e8;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] < min) {
                 min = arr[i];
@@ -42,37 +44,3 @@ public class Main {
     }
 
 }
-
-// Questions :
-// SPAN OF ARRAY :
-// import java.util.Scanner;
-
-// public class Main {
-// // span of array : max - min
-// // diffrence of max and min
-// public static int spanOfArray(int[] arr) {
-// int max = Integer.MIN_VALUE;
-// int min = Integer.MAX_VALUE;
-// for(int i = 0; i < arr.length; i++) {
-// if(arr[i] < min) {
-// min = arr[i];
-// }
-// if(arr[i] > max) {
-// max = arr[i];
-// }
-// }
-// return (max - min);
-// }
-
-// public static void main(String[] args) {
-// Scanner sc = new Scanner(System.in);
-// int n = sc.nextInt();
-// int[] arr = new int[n];
-// for (int i = 0; i < n; i++) {
-// arr[i] = sc.nextInt();
-// }
-// System.out.println(spanOfArray(arr));
-// sc.close();
-// }
-
-// }
