0c1f483b8e4c913f14804e7ce02e9a6d18a75eda
Milind Mishra <milind.mishra4@gmail.com>
2021-12-02 22:06:25 +0530

    rotate array docs

---
+++ lecture-013/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        rotate(arr, k);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        sc.close();
+    }
+
+    public static void rotate(int[] arr, int k) {
+        k = (k > 0) ? k % arr.length : (k % arr.length) + arr.length;
+        reverse(arr, 0, arr.length - k - 1);
+        reverse(arr, arr.length - k, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
