826a0d9e3f610c7e2552dca821aed99815556ece
Milind Mishra <milind.mishra4@gmail.com>
2021-12-02 22:43:26 +0530

    inverse array

--- lecture-013/Main.java
+++ lecture-013/Main.java
@@ -1,27 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int k = sc.nextInt();
-        rotate(arr, k);
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        sc.close();
-    }
-
-    public static void rotate(int[] arr, int k) {
-        k = (k > 0) ? k % arr.length : (k % arr.length) + arr.length;
-        reverse(arr, 0, arr.length - k - 1);
-        reverse(arr, arr.length - k, arr.length - 1);
-        reverse(arr, 0, arr.length - 1);
-    }
 
     public static void reverse(int[] arr, int start, int end) {
         while (start < end) {
@@ -31,5 +10,42 @@ public class Main {
             start++;
             end--;
         }
+    }
+
+    public static void rotate(int[] arr, int k) {
+        k = (k > 0) ? k % arr.length : (k % arr.length) + arr.length;
+        reverse(arr, 0, arr.length - k - 1);
+        reverse(arr, arr.length - k, arr.length - 1);
+        reverse(arr, 0, arr.length - 1);
+    }
+
+    public static int[] inverse(int[] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int val = arr[i];
+            ans[val] = i;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        // int k = sc.nextInt();
+        // rotate(arr, k);
+        // for (int i = 0; i < n; i++) {
+        // System.out.print(arr[i] + " ");
+        // }
+
+        int[] ans = inverse(arr);
+        for (int i = 0; i < n; i++) {
+            System.out.print(ans[i] + " ");
+        }
+
+        sc.close();
     }
 }
