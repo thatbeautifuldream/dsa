43c8370fed011a0b5f7517ebac1fe78514826761
Milind Mishra <milind.mishra4@gmail.com>
2021-12-03 21:48:32 +0530

    binary search done

--- lecture-014/Main.java
+++ lecture-014/Main.java
@@ -22,13 +22,29 @@ public class Main {
     // 6. If the target is not found, return -1
 
     public static int binarySearch(int[] arr, int target) {
-
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
+        System.out.println(binarySearch(arr, target));
         sc.close();
     }
 }
