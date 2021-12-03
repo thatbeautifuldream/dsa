2257df7a576ffda990f6499768666fa7f9da6c61
Milind Mishra <milind.mishra4@gmail.com>
2021-12-03 21:24:18 +0530

    binary search

--- lecture-014/Main.java
+++ lecture-014/Main.java
@@ -8,28 +8,21 @@ public class Main {
     // First and last index
     // Subset
 
-    // Binary Search :
-    // 1. Find the middle element of the array
-    // 2. Compare the middle element with the target
+    // Binary Search (BS) :
+    // works only on sorted arrays (treat this as a hint to solve a problem)
+    // to work on unsorted array, we need to sort to apply BS
+    //
+    // 1. Find the middle element of the array : mid = (start + end)/2
+    // 2. Compare the middle element with the target : target -?- mid
     // 3. If the target is greater, search the right half of the array
+    // target > mid ? start = mid + 1 : end = mid - 1
     // 4. If the target is smaller, search the left half of the array
     // 5. If the target is equal, return the index of the target
+    // target == mid : return mid
     // 6. If the target is not found, return -1
 
     public static int binarySearch(int[] arr, int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) {
-                return mid;
-            } else if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return -1;
+
     }
 
     public static void main(String[] args) {
