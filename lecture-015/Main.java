c8f02f699813599b8372a84fb20b810fd425c32d
Milind Mishra <milind.mishra4@gmail.com>
2021-12-04 23:19:23 +0530

    2d matrix mult

--- lecture-015/Main.java
+++ lecture-015/Main.java
@@ -96,6 +96,22 @@ public class Main {
         }
     }
 
+    public static void matrixMultiplication(int[][] arr1, int[][] arr2) {
+        if (arr1[0].length != arr2.length) {
+            System.out.println("Invalid matrix multiplication");
+            return;
+        }
+        int[][] result = new int[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        print2DArray(result);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // int n = sc.nextInt();
@@ -104,15 +120,34 @@ public class Main {
         // arr[i] = sc.nextInt();
         // }
         // printSubsets(arr);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[][] arr = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                arr[i][j] = sc.nextInt();
+
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
+        // int[][] arr = new int[n][m];
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < m; j++) {
+        // arr[i][j] = sc.nextInt();
+        // }
+        // }
+        // print2DArray(arr);
+
+        int n1 = sc.nextInt();
+        int m1 = sc.nextInt();
+        int[][] arr1 = new int[n1][m1];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                arr1[i][j] = sc.nextInt();
             }
         }
-        print2DArray(arr);
+        int n2 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int[][] arr2 = new int[n2][m2];
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+        matrixMultiplication(arr1, arr2);
         sc.close();
     }
 }
