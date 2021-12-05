96162a1c67a9c21ee333ef3f520f00f77ddaca53
Milind Mishra <milind.mishra4@gmail.com>
2021-12-05 08:13:45 +0530

    changes

--- lecture-015/Main.java
+++ lecture-015/Main.java
@@ -104,9 +104,11 @@ public class Main {
         int[][] result = new int[arr1.length][arr2[0].length];
         for (int i = 0; i < arr1.length; i++) {
             for (int j = 0; j < arr2[0].length; j++) {
+                int sum = 0;
                 for (int k = 0; k < arr2.length; k++) {
-                    result[i][j] += arr1[i][k] * arr2[k][j];
+                    sum += arr1[i][k] * arr2[k][j];
                 }
+                result[i][j] = sum;
             }
         }
         print2DArray(result);
@@ -149,14 +151,6 @@ public class Main {
         // }
         // matrixMultiplication(arr1, arr2);
 
-        int n = sc.nextInt();
-        int[][] arr = new int[n][n];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                arr[i][j] = sc.nextInt();
-            }
-        }
-        waveArray(arr);
         sc.close();
     }
 }
