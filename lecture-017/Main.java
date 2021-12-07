09d3fda153f2df4d1f6117448425c0d80dbb3357
Milind Mishra <milind.mishra4@gmail.com>
2021-12-07 18:54:39 +0530

    template added

---
+++ lecture-017/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        sc.close();
+    }
+}
