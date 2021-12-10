9c65b85cdd809d4dfcce60f197c9d0c49063c1b1
Milind Mishra <milind.mishra4@gmail.com>
2021-12-10 21:41:06 +0530

    initial commit

--- lecture-020/Main.java
+++ lecture-020/Main.java
@@ -1,6 +1,25 @@
 import java.util.Scanner;
 
 public class Main {
+
+    // String Compression :
+    // 1st function : aabcccccaaa -> abca
+    // 2nd function : aaabbccdddda -> a3b2c2d4a
+
+    public static String compression1(String s) {
+        String ans = "";
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                ans += s.charAt(i);
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(compression1(s));
+        sc.close();
     }
 }
