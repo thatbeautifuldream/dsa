5d2430927cf3672118e0e60f13b1a8c2e6d4b2cf
Milind Mishra <milind.mishra4@gmail.com>
2021-12-13 22:35:49 +0530

    changes

--- lecture-022/Main.java
+++ lecture-022/Main.java
@@ -27,6 +27,28 @@ public class Main {
     // printDecreasing(0) -> 5 4 3 2 1 0
     // base case reached : return and stop recursion : pops all the way up
 
+    public static void printIncreasing(int n) {
+        if (n == 0) {
+            return;
+        }
+        printIncreasing(n - 1);
+        System.out.println(n);
+    }
+
+    // Memory Stack :
+    // printIncreasing(5) -> gets pushed to the stack
+    // printIncreasing(4) -> push
+    // printIncreasing(3) -> push
+    // printIncreasing(2) -> push
+    // printIncreasing(1) -> push
+    // printIncreasing(0) -> base case reaches so it returns and stops recursion,
+    // pops all the way up
+    // printIncreasing(1) -> prints 1 and pops up
+    // printIncreasing(2) -> prints 1 2 and pops up
+    // printIncreasing(3) -> prints 1 2 3 and pops up
+    // printIncreasing(4) -> prints 1 2 3 4 and pops up
+    // printIncreasing(5) -> prints 1 2 3 4 5 and pops up
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
