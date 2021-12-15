0acd7410260d4a976f5ae818cf7216b827dd7563
Milind Mishra <milind.mishra4@gmail.com>
2021-12-15 23:04:40 +0530

    d arr

--- lecture-024/Main.java
+++ lecture-024/Main.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+    // Agenda
     // tower of hanoi
+    // Recursion in arrays
 
     public static void toh(int n, int t1, int t2, int t3) {
         if (n == 0) {
@@ -12,13 +14,26 @@ public class Main {
         toh(n - 1, t3, t2, t1);
     }
 
+    public static void displayArray(int[] a, int i) {
+        if (i == a.length) {
+            return;
+        }
+        System.out.println(a[i]);
+        displayArray(a, i + 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int t1 = sc.nextInt();
-        int t2 = sc.nextInt();
-        int t3 = sc.nextInt();
-        toh(n, t1, t2, t3); // t1 -> t2 using t3
+        // int t1 = sc.nextInt();
+        // int t2 = sc.nextInt();
+        // int t3 = sc.nextInt();
+        // toh(n, t1, t2, t3); // t1 -> t2 using t3
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        displayArray(a, 0);
         sc.close();
     }
 }
