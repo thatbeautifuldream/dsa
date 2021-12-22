76b63ecd08743103de1d0f7fa0cf437f228ab659
Milind Mishra <milind.mishra4@gmail.com>
2021-12-22 23:25:07 +0530

    get stair paths

---
+++ lecture-027/Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<String> ans = getStairPaths(n);
+        System.out.println(ans);
+        sc.close();
+    }
+
+    public static ArrayList<String> getStairPaths(int n) {
+        if (n == 0) { // base case
+            ArrayList<String> base = new ArrayList<String>();
+            base.add("");
+            return base;
+        }
+        ArrayList<String> ans = new ArrayList<String>();
+        if (n - 1 >= 0) {
+            ArrayList<String> nm1 = getStairPaths(n - 1); // get all possible paths for n-1 or 1 step
+            for (String s : nm1) {
+                ans.add("1" + s);
+            }
+        }
+        if (n - 2 >= 0) {
+            ArrayList<String> nm2 = getStairPaths(n - 2); // get all possible paths for n-2 or 2 step
+            for (String s : nm2) {
+                ans.add("2" + s);
+            }
+        }
+        if (n - 3 >= 0) {
+            ArrayList<String> nm3 = getStairPaths(n - 3); // get all possible paths for n-3 or 3 step
+            for (String s : nm3) {
+                ans.add("3" + s);
+            }
+        }
+        return ans;
+    }
+}
