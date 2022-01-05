bb0c38e1331e00a6ee8ed5ff4c11a9dc762b4e39
milind <milind.mishra4@gmail.com>
2022-01-05 21:03:49 +0530

    new

--- lecture-033/Main.java
+++ lecture-033/Main.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main {
 
-    // !Agenda
+    // !Agenda for today:
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         sc.close();
