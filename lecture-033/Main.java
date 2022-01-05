643bb69c66c1a5ebe29f5d14c4741a28a6cd7df0
milind <milind.mishra4@gmail.com>
2022-01-05 21:52:26 +0530

    new

--- lecture-033/Main.java
+++ lecture-033/Main.java
@@ -3,8 +3,26 @@ import java.util.*;
 public class Main {
 
     // !Agenda for today:
+    // Knights Tour
+
+    public static void printKnightsTour(int[][] chess, int r, int c, int upcomingMove) {
+
+    }
+
+    public static void displayBoard(int[][] chess) {
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[0].length; j++) {
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("1");
         sc.close();
     }
 }
