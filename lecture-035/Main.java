f8e768b3c836551654d6b9434a2a6b1c3483b36c
Milind Mishra <milind.mishra4@gmail.com>
2022-01-10 23:02:59 +0530

    new

--- lecture-035/Main.java
+++ lecture-035/Main.java
@@ -130,6 +130,23 @@ public class Main {
                 size++;
             }
         }
+
+        public void removeLast() {
+            if (size == 0) {
+                System.out.println("List is empty");
+            } else if (size == 1) {
+                head = tail = null;
+                size = 0;
+            } else {
+                Node temp = head;
+                for (int i = 0; i < size - 2; i++) {
+                    temp = temp.next;
+                }
+                tail = temp;
+                tail.next = null;
+                size--;
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -239,5 +256,44 @@ public class Main {
         // str = br.readLine();
         // }
         // !REMOVE LAST IN LINKED LIST
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("addLast")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addLast(val);
+            } else if (str.startsWith("size")) {
+                System.out.println(list.size());
+            } else if (str.startsWith("display")) {
+                list.display();
+            } else if (str.startsWith("removeFirst")) {
+                list.removeFirst();
+            } else if (str.startsWith("getFirst")) {
+                int val = list.getFirst();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("getLast")) {
+                int val = list.getLast();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("getAt")) {
+                int idx = Integer.parseInt(str.split(" ")[1]);
+                int val = list.getAt(idx);
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("addFirst")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                list.addFirst(val);
+            } else if (str.startsWith("addAt")) {
+                int idx = Integer.parseInt(str.split(" ")[1]);
+                int val = Integer.parseInt(str.split(" ")[2]);
+                list.addAt(idx, val);
+            } else if (str.startsWith("removeLast")) {
+                list.removeLast();
+            }
+            str = br.readLine();
+        }
     }
 }
