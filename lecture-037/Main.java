40b114de59f0a44e3c64a632c24a65e519386002
Milind Mishra <milind.mishra4@gmail.com>
2022-01-14 21:07:26 +0530

    new

---
+++ lecture-037/Main.java
@@ -0,0 +1,11 @@
+// !Agenda
+// 
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.close();
+    }
+}
