69bd0a80b7f3f98bf18f99ed137d3bd0628c9f8d
Milind Mishra <milind.mishra4@gmail.com>
2022-01-21 22:31:06 +0530

    👌 IMPROVE: dp

--- lecture-040/Main.java
+++ lecture-040/Main.java
@@ -1,9 +1,41 @@
 
+// Agenda : Dynamic Programming
+// Fibonacci-dp
+
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // System.out.println(fiboR(n));
+        int[] dp = new int[n + 1]; // stores values of fibonacci numbers from 0 to n => (n+1) terms
+        Arrays.fill(dp, -1);
+        System.out.println(fiboM(n, dp));
         sc.close();
     }
+
+    // recursive solution
+    public static int fiboR(int n) {
+        if (n <= 1) {
+            return n;
+        } else {
+            return fiboR(n - 1) + fiboR(n - 2);
+        }
+    }
+
+    // memoization : stores values of fiboR(n)
+    public static int fiboM(int n, int[] dp) {
+        if (n <= 1) {
+            return dp[n] = n;
+        } else {
+            if (dp[n] == -1) {
+                return dp[n] = fiboM(n - 1, dp) + fiboM(n - 2, dp);
+            }
+            return dp[n];
+        }
+    }
+
 }
