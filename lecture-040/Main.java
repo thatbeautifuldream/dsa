7fa483fa654a93d089d7059c5dcdc5b32d717bc0
Milind Mishra <milind.mishra4@gmail.com>
2022-01-21 23:00:36 +0530

    📦 NEW: tabulation

--- lecture-040/Main.java
+++ lecture-040/Main.java
@@ -7,13 +7,21 @@ import java.util.*;
 
 public class Main {
 
+    public static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         // System.out.println(fiboR(n));
         int[] dp = new int[n + 1]; // stores values of fibonacci numbers from 0 to n => (n+1) terms
-        Arrays.fill(dp, -1);
-        System.out.println(fiboM(n, dp));
+        Arrays.fill(dp, -1); // -1 is used to represent uninitialized values
+        System.out.println(fiboM(n, dp)); // spits nth fibonacci number
+        display(dp); // spits the fibonacci series
         sc.close();
     }
 
@@ -38,4 +46,15 @@ public class Main {
         }
     }
 
+    // tabulation
+    public static int fiboT(int N, int[] dp) {
+        for (int n = 0; n < dp.length; n++) {
+            if (n <= 1) {
+                dp[n] = n;
+                continue;
+            }
+            dp[n] = dp[n - 1] + dp[n - 2];
+        }
+        return dp[N];
+    }
 }
--- lecture-040/README.md
+++ lecture-040/README.md
@@ -1,1 +1,3 @@
-# Agenda |
+# Agenda | Dynamic Programming
+
+## [Fibonacci-dp](https://thatbeautifuldream.github.io/pepcoding-dsa/lecture-040/fibonacci-dp.html)
