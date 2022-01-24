b284678f4cc2e974b5fb7a3263710564a8f9d490
Milind Mishra ⚡️ <milind.mishra4@gmail.com>
2022-01-24 22:00:06 +0530

    👌 IMPROVE: memoize

--- lecture-041/Main.java
+++ lecture-041/Main.java
@@ -1,11 +1,54 @@
-// Agenda | Dynamic Programming
-// 
 
+// Agenda | Dynamic Programming
+// Climb Stairs With Variable Jumps
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static int solve(int[] a, int src, int dest) {
+        if (src == dest)
+            return 1;
+        int count = 0;
+        for (int jump = 1; jump <= a[src] && src + jump <= dest; jump++) {
+            count += solve(a, src + jump, dest); // new src = src + jump
+        }
+        return count;
+    }
+
+    public static int solveM(int[] a, int src, int dest, int[] dp) {
+        if (src == dest)
+            return dp[src] = 1;
+        if (dp[src] != 0)
+            return dp[src];
+        int count = 0;
+        for (int jump = 1; jump <= a[src] && src + jump <= dest; jump++) {
+            count += solveM(a, src + jump, dest, dp);
+        }
+        return dp[src] = count;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // Climb stairs with variable jumps
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int[] a = new int[n];
+        // for (int i = 0; i < n; i++) {
+        // a[i] = sc.nextInt();
+        // }
+        // int ans = solve(a, 0, n); // arr, src, dest
+        // System.out.println(ans);
+
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] dp = new int[n + 1];
+        int ans = solveM(a, 0, n, dp);
+        System.out.println(ans);
         sc.close();
     }
+
 }
