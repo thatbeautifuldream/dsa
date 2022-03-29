6915ff499f157c3a0287a46ce90f8e38fa476f80
Milind Mishra <milind.mishra4@gmail.com>
2022-03-29 11:24:45 +0530

    📦 NEW: added

--- lecture-060/144. Binary Tree Preorder Traversal/Main.java
+++ lecture-060/144. Binary Tree Preorder Traversal/Main.java
@@ -1,8 +1,8 @@
-public /**
-        * Definition for a binary tree node. public class TreeNode { int val; TreeNode left;
-        * TreeNode right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val,
-        * TreeNode left, TreeNode right) { this.val = val; this.left = left; this.right = right; } }
-        */
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode() {} TreeNode(int val) { this.val = val; } TreeNode(int val, TreeNode left,
+ * TreeNode right) { this.val = val; this.left = left; this.right = right; } }
+ */
 class Solution {
     public void preOrder(TreeNode root, List<Integer> ls) {
         if (root == null)
